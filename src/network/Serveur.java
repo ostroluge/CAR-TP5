313c734c3b394b11ab82da3fcec2ab4071b2d776
Flamen <flamen.julien@gmail.com>
2016-06-16 10:45:18 +0200

    Ajout serveur

--- src/network/Serveur.java
+++ src/network/Serveur.java
@@ -3,23 +3,39 @@ package network;
 import java.rmi.Naming;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import domain.Repertoire;
 import service.Noeud;
+import service.impl.NoeudImpl;
 
 public class Serveur {
 
     public static void main(String[] args) throws Exception {
 
-	Noeud noeudDistant = (Noeud) Naming.lookup("//localhost/Node");
+	Repertoire rep = new Repertoire();
+	NoeudImpl noeud = new NoeudImpl(rep);
+	
+	System.out.println("T'ES LE CHEF OU PAS ???!!!");
+	Scanner sc = new Scanner(System.in);
+	
+	String str = sc.nextLine();
+	
+	if(str.equals("oui")){
+	    Naming.rebind("Node", noeud);
+	}else{
+	    Noeud noeudDistant = (Noeud) Naming.lookup("//localhost/Node");
+		List<Noeud> nodes = new ArrayList<>();
 
-	List<Noeud> nodes = new ArrayList<>();
+		nodes.clear();
 
-	nodes.clear();
-
-	nodes = noeudDistant.getLesNoeuds();
-
-	for (Noeud noeud : nodes) {
-	    System.out.println(noeud);
+		nodes = noeudDistant.getLesNoeuds();
+		for (Noeud noeud1 : nodes) {
+		    System.out.println(noeud1);
+		}
 	}
+
+
+	
     }
 }
