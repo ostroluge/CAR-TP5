d67dba9965663c5f93d8d673fa905c8d5c35e57d
Flamen <flamen.julien@gmail.com>
2016-06-16 12:03:06 +0200

    modif

--- src/network/Serveur.java
+++ src/network/Serveur.java
@@ -11,60 +11,70 @@ import service.impl.NoeudImpl;
 
 public class Serveur {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		Repertoire rep = new Repertoire();
-		NoeudImpl noeud = new NoeudImpl(rep);
+	Repertoire rep = new Repertoire();
+	NoeudImpl noeud = new NoeudImpl(rep);
 
-		System.out.println("T'ES LE CHEF OU PAS ???!!!");
-		Scanner sc = new Scanner(System.in);
+	System.out.println("T'ES LE CHEF OU PAS ???!!!");
+	Scanner sc = new Scanner(System.in);
 
-		String str = sc.nextLine();
+	String str = sc.nextLine();
 
-		if(str.equals("oui")) {
-			Naming.rebind("Node", noeud);
-		} else {
-			Noeud noeudDistant = (Noeud) Naming.lookup("//localhost/Node");
-			List<Noeud> nodes = new ArrayList<>();
+	if(str.equals("oui")) {
+	    Naming.rebind("Node", noeud);
+	} else {
+	    Noeud noeudDistant = (Noeud) Naming.lookup("//localhost/Node");
+	    List<Noeud> nodes = new ArrayList<>();
 
-			nodes.clear();
+	    nodes.clear();
 
-			System.out.println("1. Ajouter un fichier");
-			System.out.println("2. Modifier un fichier");
-			System.out.println("3. Supprimer un fichier");
 
-			Scanner scan = new Scanner(System.in);
-			String input = sc.nextLine();
+	    boolean stop = false;
 
-			switch(input) {
-			case "1":
-				System.out.println("Veuillez saisir le nom du fichier à ajouter : ");
-				String fileToAdd = sc.nextLine();
-				if (noeud.repertoire.addFichier(fileToAdd)) {
-					System.out.println("Le fichier " + fileToAdd + "a été ajouté avec succes");
-				} else {
-					System.out.println("Echec de l'ajout du fichier " + fileToAdd);
-				}
-				
-				break;
-			case "2":
-				System.out.println("Veuillez saisir le nom du fichier à modifier : ");
-				break;
-			case "3":
-				System.out.println("Veuillez saisir le nom du fichier à supprimer : ");
-				break;
-			default:
-				System.out.println("Commande Inconnue");
-			}
+	    while(!stop){
+		System.out.println("1. Ajouter un fichier");
+		System.out.println("2. Modifier un fichier");
+		System.out.println("3. Supprimer un fichier");
+		System.out.println("4. Lister les fichiers");
 
-			nodes = noeudDistant.getLesNoeuds();
-			for (Noeud noeud1 : nodes) {
-				System.out.println(noeud1);
-			}
-			
-			for (String files : noeud.repertoire.fichiers) {
-				System.out.println(files);
-			}
+		Scanner scan = new Scanner(System.in);
+		String input = sc.nextLine();
+		 
+		switch(input) {
+		case "1":
+		    System.out.println("Veuillez saisir le nom du fichier à ajouter : ");
+		    String fileToAdd = sc.nextLine();
+		    if (noeud.repertoire.addFichier(fileToAdd)) {
+			System.out.println("Le fichier " + fileToAdd + " a été ajouté avec succes");
+		    } else {
+			System.out.println("Echec de l'ajout du fichier " + fileToAdd);
+		    }
+
+		    break;
+		case "2":
+		    System.out.println("Veuillez saisir le nom du fichier à modifier : ");
+		    break;
+		case "3":
+		    System.out.println("Veuillez saisir le nom du fichier à supprimer : ");
+		    break;
+		case "4":
+		    for (String files : noeud.repertoire.fichiers) {
+			System.out.println(files);
+		    }
+		    break;
+		default:
+		    stop=true;
+		    System.out.println("Commande Inconnue");
+		    break;
 		}
+
+		nodes = noeudDistant.getLesNoeuds();
+		for (Noeud noeud1 : nodes) {
+		    System.out.println(noeud1);
+		}
+
+	    }
 	}
+    }
 }
